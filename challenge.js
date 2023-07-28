bf9a1cbb5559597151b578c96a0b3454e87d55f9
MaryannAY <maryann.ahmed2@gmail.com>
2023-07-28 08:25:47 -0700

    Solution to calculator

--- challenge.js
+++ challenge.js
@@ -1,2 +1,13 @@
 // Good Luck! You got this 💪🏾
 // Write your code here.
+function calculator (num1,num2,operator){
+if (operator= `+`, `-`, `*`,`/` ) {
+    console.log(num1 + num2)
+    console.log(num1 - num2)
+    console.log(num1 * num2)
+    console.log(num1 / num2)
+} 
+}
+
+calculator(4,4)
+
